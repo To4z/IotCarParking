66eb3ee2c3c592971ee654176e4d2eec556e17d8
Toan Quoc Tran Doan <95047974+To4z@users.noreply.github.com>
2025-06-01 23:12:18 +0700

    ✨ Clean project: ignore yolov5, model files

---
+++ lp_image.py
@@ -0,0 +1,40 @@
+# lp_image.py
+from PIL import Image
+import cv2
+import torch
+import function.utils_rotate as utils_rotate
+import function.helper as helper
+
+# Load mô hình 1 lần khi import
+yolo_LP_detect = torch.hub.load('yolov5', 'custom', path='model/LP_detector.pt', force_reload=True, source='local')
+yolo_license_plate = torch.hub.load('yolov5', 'custom', path='model/LP_ocr.pt', force_reload=True, source='local')
+yolo_license_plate.conf = 0.60
+
+def detect_plates_from_image(img):
+    plates = yolo_LP_detect(img, size=640)
+    list_plates = plates.pandas().xyxy[0].values.tolist()
+    list_read_plates = set()
+
+    if len(list_plates) == 0:
+        lp = helper.read_plate(yolo_license_plate, img)
+        if lp != "unknown":
+            list_read_plates.add(lp)
+    else:
+        for plate in list_plates:
+            flag = 0
+            x = int(plate[0])
+            y = int(plate[1])
+            w = int(plate[2] - plate[0])
+            h = int(plate[3] - plate[1])
+            crop_img = img[y:y+h, x:x+w]
+
+            for cc in range(0, 2):
+                for ct in range(0, 2):
+                    lp = helper.read_plate(yolo_license_plate, utils_rotate.deskew(crop_img, cc, ct))
+                    if lp != "unknown":
+                        list_read_plates.add(lp)
+                        flag = 1
+                        break
+                if flag == 1:
+                    break
+    return list(list_read_plates)
